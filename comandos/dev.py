7903a7bc36e85c11e239e9b59ef24854bce9c752
vinicgobbi <vinicius.cgobbi2004@gmail.com>
2023-06-03 21:31:54 -0300

    Fix the link for the repo on docs command

--- comandos/dev.py
+++ comandos/dev.py
@@ -9,7 +9,7 @@ async def docs(ctx, arg):
         lang = ' '.join(arg).lower()
         if lang not in dados:
             await ctx.send(f"Linguagem {lang} não encontrada, aguarde que ela pode ser adicionada no futuro")
-            await ctx.send(f"Se quiser pode colaborar adicionando novas linguagens e comandos ao bot [Aqui](https://github.com/vinicgobbi/vini-bot-python)")
+            await ctx.send(f"Se quiser pode colaborar adicionando novas linguagens e comandos ao bot Aqui: https://github.com/vinicgobbi/vini-bot-py")
         else:
             link = dados[lang]
             await ctx.send(f"Estude a linguagem {lang} pelo site {link}")
